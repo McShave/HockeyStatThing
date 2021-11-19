194272246efc73b52e2fde59809fdbeb19b40f1a
Mitchell Schaefer <mdschaefer24@gmail.com>
2021-11-19 12:36:23 -0500

    added comments. fixed some bugs and issues with the math

--- HockeyStats.py
+++ HockeyStats.py
@@ -3,42 +3,62 @@ import sys
 
 import json
 
-from HockeyStatsFunctions import newTeamList
-from HockeyStatsFunctions import updateGameRange
-from HockeyStatsFunctions import convertJsonToCSV
+from HockeyStatsFunctions import *
 
 def main(argv):
 
+    # variables
     jsonFileName = "teamList.json"
     csvFileName = "teamList.csv"
 
-    # if teamList.json does not exist, create new file and teamList
+    # if jsonFileName does not exist in current directory,
+    # create new file with a basic list of teams
     if not os.path.isfile("teamList.json") or os.path.getsize("teamList.json") == 0:
+        
         print("Creating new json file")
-        with open("teamList.json", "w") as teamListFile:
+        
+        # open jsonFileName as teamListFile, if jsonFileName does not exst, create it (with "w")
+        # call newTeamList() from HockeyStatsFunctions.py, function returns json object
+        # convert json object teamList to string with json.dumps(),
+        # write the object to the opened file
+        # close the file
+        with open(jsonFileName, "w") as teamListFile:
             teamList = newTeamList()
             teamListInfo = json.dumps(teamList, indent=4)
             teamListFile.write(teamListInfo)
             teamListFile.close()
 
-    # open teamList.json
-    teamListFile = open("teamList.json", "r+")
-    teamListInfo = teamListFile.read()
-    teamList = json.loads(teamListInfo)
-    
-    # for team in teamList:
-        # print(teamList[team])
-        
-    teamList = updateGameRange(teamList, int(argv[0]), int(argv[1]))
+    # open jsonFileName as TeamListFile, opens for read (with "r")
+    # read teamListFile to string teamListInfo
+    # convert file string to json object
+    # close file
+    with open(jsonFileName, "r") as teamListFile:
+        teamListInfo = teamListFile.read()
+        teamList = json.loads(teamListInfo)
+        teamListFile.close()    
 
+    # variables
+    num1 = int(argv[0])
+    num2 = int(argv[1])
+    
+    # call function updateGameRange() from HockeyStatFunctions.py
+    # returns an updated teamList with games with Game IDs from num1 to num2
+    teamList = updateGameRange(teamList, num1, num2)
+
+    # call function convertJsonToCSV() from HockeyStatFunctions.py
+    # writes teamList["teams"] as a CSV file called csvFileName
     convertJsonToCSV(csvFileName, teamList["teams"])
 
-    #update teamList and write to teamList.json
-    teamListInfo = json.dumps(teamList, indent=4)
-    teamListFile.seek(0)
-    teamListFile.write(teamListInfo)
-    teamListFile.close()
-
+    # open jsonFileName as TeamListFile, opens for read and write (with "r+")
+    # convert json object to string
+    # write json string to file
+    # close file
+    with open(jsonFileName, "r+") as teamListFile:
+        teamListInfo = json.dumps(teamList, indent=4)
+        teamListFile.write(teamListInfo)
+        teamListFile.close()
+        
+# call main()
 if __name__ == '__main__':
     main(sys.argv[1:])
     
